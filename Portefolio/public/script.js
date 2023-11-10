eed093879b08a5333cd3bced25d70ed1b1f35c3e
Nico-dsa <6463@holbertonstudenst.com>
2023-11-10 12:45:10 +0100

    all

--- Portefolio/public/script.js
+++ Portefolio/public/script.js
@@ -6,7 +6,6 @@ document.getElementById('imageUpload').addEventListener('change', function(event
     reader.readAsDataURL(event.target.files[0]);
 });
 
-// Vous pouvez ajouter ici des fonctions pour gérer les objets à vendre, vendus et achetés.
 
 function editField(fieldId) {
     var field = document.getElementById(fieldId);
@@ -14,9 +13,7 @@ function editField(fieldId) {
     field.focus(); // Mettre le focus sur le champ
   }
   
-  // Optionnel: Vous pouvez également ajouter un écouteur d'événement pour le bouton de mise à jour
   document.getElementById('updateProfile').addEventListener('click', function() {
-    // Ici, vous pourriez valider et soumettre les données mises à jour
     var inputs = document.querySelectorAll('.profile-info input');
     inputs.forEach(function(input) {
       input.disabled = true; // Désactiver à nouveau les champs après la mise à jour
